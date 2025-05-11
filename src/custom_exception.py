a81a104e15530e6e7de28fb7157d7ebd9a9234be
neerajprasad209 <neerajprasad.209@gmail.com>
2025-05-11 23:06:50 +0530

    add jenkins files

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -9,7 +9,7 @@ class custom_exception(Exception):
                             
     @staticmethod
     def get_detailed_error_message(error_messsage, error_detail:sys):
-        _,_, exc_tb = error_detail.exc_info()
+        _,_, exc_tb = traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
         error_message = f"Error occurred in script: {file_name} at line number: {line_number} error message: {error_messsage}"
